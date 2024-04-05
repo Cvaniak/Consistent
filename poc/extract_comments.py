871ad1cb84e981174452faabc62a331734a595bf
Cvaniak <igna.cwaniak@gmail.com>
2024-04-05 17:32:00 +0200

    :alembic: Refactor extract comments

--- poc/extract_comments.py
+++ poc/extract_comments.py
@@ -1,48 +1,68 @@
+from dataclasses import dataclass, asdict
 import json
+from typing import List, Tuple
 from tree_sitter import Language, Node, Parser
 import git
 import bisect
 
+
+@dataclass
+class Comment:
+    start: Tuple[int, int]
+    text: str
+
+
+@dataclass
+class CommentsStruct:
+    comments: List[Comment]
+    deleted_lines: List[int]
+    commit_sha: str
+    file_name: str
+
+
 # Load the language library (Adjust the path to your compiled language library)
-Language.build_library(
-    "build/my-languages.so",
-    ["tree-sitter-python"],  # Assuming the grammar files are correctly configured
-)
+def get_tree(source_code):
+    Language.build_library(
+        "build/my-languages.so",
+        ["tree-sitter-python"],  # Assuming the grammar files are correctly configured
+    )
 
-PY_LANGUAGE = Language("build/my-languages.so", "python")
-parser = Parser()
-parser.set_language(PY_LANGUAGE)
+    PY_LANGUAGE = Language("build/my-languages.so", "python")
+    parser = Parser()
+    parser.set_language(PY_LANGUAGE)
 
-
-def remove_comments(source_code, lines):
     tree = parser.parse(bytes(source_code, "utf8"))
-    edit_ranges = []
+    return tree
 
-    # Collect ranges for comments
-    def collect_comment_ranges(node: Node):
+
+def collect_comment_nodes(tree):
+    comment_nodes = []
+
+    def _collect_comment_ranges(node: Node):
         if node.type == "comment":
-            edit_ranges.append(node)
+            comment_nodes.append(node)
         else:
             for child in node.children:
-                collect_comment_ranges(child)
+                _collect_comment_ranges(child)
 
-    collect_comment_ranges(tree.root_node)
+    _collect_comment_ranges(tree.root_node)
 
-    # This is just test format
-    just_comments = {
-        "comments": [],
-        "deleted_lines": [],
-    }
+    return comment_nodes
+
+
+def remove_comments(tree, lines):
+    # Collect ranges for comments
+    comment_nodes = collect_comment_nodes(tree)
+
+    comments = []
+    deleted_lines = []
 
     # Remove comments by replacing them with spaces (to preserve formatting)
-    for node in reversed(edit_ranges):  # Reverse to avoid offset issues
-        start_b, end_b = node.start_byte, node.end_byte
+    for node in reversed(comment_nodes):  # Reverse to avoid offset issues
         start_p, end_p = node.start_point, node.end_point
 
-        comment = {}
-        comment["start"] = node.start_point
-        comment["text"] = source_code[start_b:end_b]
-        just_comments["comments"].append(comment)
+        comment = Comment(start=start_p, text=lines[start_p[0]][start_p[1] : end_p[1]])
+        comments.append(comment)
 
         # TODO: here was edge case, needs to be watched for more
         if (
@@ -50,36 +70,45 @@ def remove_comments(source_code, lines):
             and node.prev_sibling.start_point[0] != start_p[0]
         ):
             lines[start_p[0]] = ""
-            bisect.insort(just_comments["deleted_lines"], start_p[0])
+            bisect.insort(deleted_lines, start_p[0])
         else:
             lines[start_p[0]] = lines[start_p[0]][: start_p[1]] + "\n"
 
-    lines = lines
+    return comments, deleted_lines, lines
 
-    return source_code, just_comments, lines
+
+def get_commit_sha():
+    repo = git.Repo(search_parent_directories=True)
+    sha = repo.head.object.hexsha
+    return sha
 
 
 # Example of reading,
 # processing, and writing the file
 # also three line comment
-input_file_path = "extract_comments.py"
-output_file_path = "main_no_comments.py"
-output_comments_file_path = "just_comments.json"
+def main(input_file_path, output_file_path, output_comments_file_path):
+    with open(input_file_path, "r", encoding="utf-8") as file:
+        source_code = file.read()
 
-with open(input_file_path, "r", encoding="utf-8") as file:
-    source_code = file.read()
+    with open(input_file_path, "r", encoding="utf-8") as file:
+        lines = file.readlines()
 
-with open(input_file_path, "r", encoding="utf-8") as file:
-    lines = file.readlines()
+    tree = get_tree(source_code)
+    comments, deleted_lines, new_lines = remove_comments(tree, lines)
 
-clean_code, comments, new_lines = remove_comments(source_code, lines)
+    out_data = CommentsStruct(
+        comments, deleted_lines, get_commit_sha(), input_file_path
+    )
 
-repo = git.Repo(search_parent_directories=True)
-sha = repo.head.object.hexsha
-comments["git_hash"] = sha
+    with open(output_file_path, "w", encoding="utf-8") as file:
+        file.writelines(new_lines)
 
-with open(output_file_path, "w", encoding="utf-8") as file:
-    file.writelines(new_lines)
+    with open(output_comments_file_path, "w", encoding="utf-8") as file:
+        json.dump(asdict(out_data), file, indent=4)
 
-with open(output_comments_file_path, "w", encoding="utf-8") as file:
-    json.dump(comments, file, indent=4)
+
+if __name__ == "__main__":
+    input_file_path = "extract_comments.py"
+    output_file_path = "main_no_comments.py"
+    output_comments_file_path = "just_comments.json"
+    main(input_file_path, output_file_path, output_comments_file_path)
